93618050073cbe21555497f03946f6b3715e784b
Sayan Halder <>
2024-04-04 19:48:22 +0530

    edit upload

--- src/main/java/com/cognizant/training/hotelmanagement/controller/RoomController.java
+++ src/main/java/com/cognizant/training/hotelmanagement/controller/RoomController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cognizant.training.hotelmanagement.model.Booking;
 import com.cognizant.training.hotelmanagement.model.Room;
 import com.cognizant.training.hotelmanagement.service.RoomService;
 import org.springframework.web.bind.annotation.RequestParam;
